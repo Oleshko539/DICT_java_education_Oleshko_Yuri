c440e87e7d63e29a041355dbf2045bea6def4595
Yuri Oleshko <y.o.oleshko@student.khai.edu>
2021-12-19 21:23:57 +0200

    Hangman 1 stage

---
+++ src/Hangman/hangman.java
@@ -0,0 +1,8 @@
+package Hangman;
+
+public class hangman {
+    public static void main(String [] arg){
+        System.out.println("HANGMAN\n" +
+                "The game will be available soon");
+    }
+}
